6525c0d951a7ca24966493378c152b5c9488eee8
brian890809 <brian08090809@gmail.com>
2025-01-15 21:16:04 -0800

    added post verify api (scaffold)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,11 @@ import express from 'express'
 const router = express.Router()
 
 import controller from '../controllers/firebase-auth-controller.js'
+import postController from '../controllers/post-controller.js'
+
+import {verifyToken} from '../middleware/index.js'
+
+router.get('/api/posts', verifyToken, postController.getPosts)
 
 router.post('/api/register', controller.registerUser);
 router.post('/api/login', controller.loginUser);
